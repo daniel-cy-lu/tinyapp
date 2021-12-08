fac9814178d0f80a0e307d665874e60bfd031848
Daniel Lu <daniel_lu@hotmail.com>
2021-12-08 14:43:27 -0500

    fixed bug when sites donot load with short /u/:shortURL

--- express_server.js
+++ express_server.js
@@ -1,11 +1,11 @@
 //Project Setup using express, bodyParser
 const express = require('express');
+const bodyParser = require("body-parser");
 const app = express();
 const PORT = 8080;
 //setup body-parser for POST request
-const bodyParser = require("body-parser");
+
 app.use(bodyParser.urlencoded({extended: true}));
-//setup ejs to render HTML
 app.set('view engine', 'ejs');
 
 //setup id object - keys are short URL and values are long URL
@@ -14,38 +14,31 @@ const urlDatabase = {
   '9sm5xK' : 'http//www.google.com'
 };
 
-//Generate six rando strings
+//Helper Function
 const generateRandomString = function() {
   let random = (Math.random() + 1).toString(36).substring(2,8);
   return random;
 };
 
-//POST request - executed after user enter url in /urls/new
-//It generate a random variable and saves it in urlDatabase then redirect user to /urls/(newly created link)
+//Add
 app.post("/urls", (req, res) => {
-  console.log(req.body);  
-        
+  
   const random = generateRandomString();
   urlDatabase[random] = req.body.longURL;
   
   res.redirect('http://localhost:8080/urls/' + random);
 });
 
-app.post('/login', (req, res) => {
-  console.log(req.body);
-  const cookie = req.body.username;
-
-  res.redirect('/urls');
-})
-
+// Edit
 app.post('/urls/:shortURL', (req, res) => {
-  editShortURLID = req.params.shortURL;
-  updatedLongURL = req.body.longURL;
-  urlDatabase[editShortURLID] = updatedLongURL;
+  const editShortURLID = req.params.shortURL;
+  const updatedLongURL = req.body.longURL;
+  urlDatabase.editShortURLID = updatedLongURL;
 
   res.redirect('/urls');
 })
 
+//Delete
 app.post('/urls/:shortURL/delete', (req, res) => {
   
   const deleteShortURLID = req.params.shortURL;
@@ -54,17 +47,21 @@ app.post('/urls/:shortURL/delete', (req, res) => {
   res.redirect('/urls');
 })
 
+//Login
+app.post('/login', (req, res) => {
+  console.log(req.body);
+  const cookie = req.body.username;
 
+  res.redirect('/urls');
+})
 
 //Ruotes using app.get:
 app.get('/', (req, res) => {
   res.send('Hello!');
 });
-// Display the urlDatabase in string
 app.get('/urls.json', (req, res) => {
   res.json(urlDatabase);
 });
-
 app.get('/hello', (req, res) => {
   res.send('<html><body>Hello <b>World</b><body></html>\n');
 });
@@ -75,13 +72,15 @@ app.get('/urls', (req,res) => {
   res.render('urls_index', templateVars);
 });
 
-//Webpage where user can enter a new long url
+//Add
 app.get('/urls/new', (req,res) => {
   res.render('urls_new');
 });
 
 // a short/u/ brings user to long url via clicking on short url
 app.get("/u/:shortURL", (req, res) => {
+  console.log('urlDatabase',urlDatabase)
+  console.log('req.paramas',req.params)
   const longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
 });
